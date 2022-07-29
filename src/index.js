21701f3cea5e549ca847f364c49326885b9486fd
Sami Bajwa <samibajwa@csu.fullerton.edu>
2022-07-28 23:03:13 -0700

    🧹 Used .map() function to get all of robots.js

--- src/index.js
+++ src/index.js
@@ -3,18 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'tachyons';
 // import App from './App';
-import Card from './Card'
+import CardList from './CardList'
 import reportWebVitals from './reportWebVitals';
 import {robots} from './robots'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-    </div>
+    <CardList robots={robots}/>
   </React.StrictMode>
 );
 
