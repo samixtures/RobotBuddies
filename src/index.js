a49d1fd5c578438eaf8594da2a2cdb706897a63e
Sami Bajwa <samibajwa@csu.fullerton.edu>
2022-07-31 23:33:52 -0700

    Added parent App component and SearchBox component

--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'tachyons';
 // import App from './App';
-import CardList from './CardList'
+import App from './App'
 import reportWebVitals from './reportWebVitals';
-import {robots} from './robots'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CardList robots={robots}/>
+    <App/>
   </React.StrictMode>
 );
 
