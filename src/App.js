195135dc4799d22d3e4c6037f5f92a71dd51fe3f
Sami Bajwa <samibajwa@csu.fullerton.edu>
2022-08-02 14:54:16 -0700

    passing a function using "state" filters w/search

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, {Component} from "react";
 import CardList from './CardList'
 import SearchBox from './SearchBox'
 import 'tachyons'
 import { robots } from './robots'
 
-const App = () => {
-    return(
-        <div className="tc">
-            <h1>RoboFriends</h1>
-            <SearchBox/>
-            <CardList robots = {robots}/>
-        </div>
-    );
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchField: ''
+        }
+    }
+
+    onSearchChange = (thingy) => {
+        console.log(thingy.target.value)
+
+        this.setState({ searchField: thingy.target.value})
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+        })
+        return(
+            <div className="tc">
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange = {this.onSearchChange}/>
+                <CardList robots = {filteredRobots}/>
+            </div>
+        );
+    }
 }
 export default App;
